99bb35accab73381a48168f4c2da4daabae8b18e
Lucas Morales <lucas@lucasem.com>
2017-04-18 02:22:31 -0400

    take input json as command-line arg

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,14 @@ extern crate serde_derive;
 mod args;
 mod codegen;
 
+use std::env;
 use codegen::CodeGen;
 
 fn main() {
-    let filename = "specs.json";
-    let s = args::parse_json(filename);
+    let filename = env::args()
+        .nth(1)
+        .unwrap_or(String::from("specs.json"));
+    let s = args::parse_json(&filename);
     let cg = CodeGen::new(s);
     println!("{}", cg.gen());
 }
