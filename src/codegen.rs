8a36ff46b07ea648b61d3e25b5ad289e91dda8d3
Lucas Morales <lucas@lucasem.com>
2017-04-20 21:26:08 -0400

    added main, usage, and headers

--- src/codegen.rs
+++ src/codegen.rs
@@ -4,7 +4,8 @@ extern crate serde_json;
 use std::io::{Read, Write};
 use regex::Regex;
 
-static PERMITTED_C_TYPES: [&'static str; 2] = ["char*", "int32"]; // TODO: support more types
+// TODO: support more types
+static PERMITTED_C_TYPES: [&'static str; 3] = ["char", "char*", "int32"];
 
 #[derive(Deserialize)]
 struct PItem {
@@ -26,13 +27,14 @@ struct NPItem {
 }
 
 impl NPItem {
-    /// generate approriate variable declarations for this argument, to be contained
-    /// in the main function.
-    fn decl(&self) -> String {
-        String::new()
+    /// declarations for the main function.
+    fn decl_main(&self) -> String {
+        format!("\t{} {};\n", self.c_type, self.c_var)
     }
-
-
+    /// declarations for the parse_args (not main) function.
+    fn decl_parse(&self) -> String {
+        format!("\tbool {}__isset = false;\n", self.c_var)
+    }
     /// generate appropriate C code for the particular argument, to be contained within the primary
     /// argument loop. Assume that c_var is an initially-null pointer to a c_type, and
     /// c_var+"__isset" is a boolean. This function should make c_var non-null if applicable, and
@@ -41,7 +43,8 @@ impl NPItem {
         let mut code = String::new();
         // TODO: There's a special case for binary args like --verbose where there's no subsequent
         // arg. Also, we should add support for --foo=bar on top of just --foo bar
-        code.push_str(&format!("\t\tif (!strcmp(argv[i], \"--{}\") && i+1<argc) {{\n", self.name));
+        code.push_str(&format!("\t\tif (!strcmp(argv[i], \"--{}\") && i+1<argc) {{\n",
+                               self.name));
         match &*self.c_type { // TODO: int arrays, string array
             "int32" => code.push_str(&format!("\t\t\t*{} = atoi(argv[++i]);\n", self.c_var)),
             "char*" => code.push_str(&format!("\t\t\t*{} = argv[++i];\n", self.c_var)),
@@ -61,10 +64,16 @@ impl NPItem {
         let mut code = String::new();
         code.push_str(&format!("\tif (!{}__isset) {{\n", self.c_var));
         if self.required.unwrap_or(false) {
-            // TODO: exit with help
-            code.push_str("\t\t//TODO: exit with help (required arg)\n")
+            code.push_str("\t\tusage(argv[0]);\n");
+            code.push_str("\t\texit(1);\n");
         } else if let Some(ref default) = self.default {
-            code.push_str(&format!("\t\t*{} = {};\n", self.c_var, default));
+            match &*self.c_type {
+                "int32" => code.push_str(&format!("\t\t*{} = {};\n", self.c_var, default)),
+                // TODO: handle quoting correctly for char* AND char
+                "char*" => code.push_str(&format!("\t\t*{} = \"{}\";\n", self.c_var, default)),
+                "char"  => code.push_str(&format!("\t\t*{} = '{}';\n", self.c_var, default)),
+                _ => ()/* impossible */,
+            }
         }
         code.push_str("\t}\n");
         code
@@ -73,10 +82,9 @@ impl NPItem {
 
 
 impl PItem {
-    /// generate approriate variable declarations for this argument, to be contained
-    /// in the main function.
+    /// declarations for the main function.
     fn decl(&self) -> String {
-        String::new()
+        format!("\t{} {};\n", self.c_type, self.c_var)
     }
 
     fn gen(&self) -> String {
@@ -138,23 +146,63 @@ impl Spec {
             }
         }
     }
-    /// creates the C function declaration of an argen function,
-    /// ending in a `{`.
-    fn func_decl(&self) -> String {
-        String::from("void populate_args(int argc, char ** argv /* TODO */) {\n")
-
+    /// creates the necessary headers in C.
+    fn c_headers(&self) -> String {
+        String::from("#include<stdlib.h>\n#include<stdio.h>\n#include<string.h>")
     }
-    /// creates the body of the argen function.
-    fn body(&self) -> String {
+    /// creates the usage function in C.
+    fn c_usage(&self) -> String {
+        // TODO: positional usage. escape double quotes in help message.
+        let positional_usage = "[TODO ...]";
+        let mut help = String::from("  -h  --help\n        print this usage and exit\n");
+        help.push_str(&self.non_positional
+                           .iter()
+                           .map(|ref npi| {
+            let mut long = String::from("  --");
+            long.push_str(&npi.name);
+            if let Some(ref aliases) = npi.aliases {
+                for alias in aliases {
+                    long.push_str("  --");
+                    long.push_str(alias);
+                }
+            }
+            let help = match npi.help {
+                Some(ref h) => {
+                    let mut hm = String::from("\n        ");
+                    hm.push_str(h);
+                    hm
+                }
+                _ => String::new(),
+            };
+            if let Some(ref short) = npi.short {
+                format!("  -{}{}{}\n", short, long, help)
+            } else {
+                format!("     {}{}\n", long, help)
+            }
+        })
+                           .collect::<String>());
+        format!(r#"static void usage(const char *progname) {{
+	printf("usage: %s [options] {}\n%s", progname, "\
+{}");
+}}
+"#,
+                positional_usage,
+                help)
+    }
+    /// creates the parse_args function in C.
+    fn c_parse_args(&self) -> String {
         let mut body = String::new();
+        body.push_str("void parse_args(int argc, char **argv /* TODO */) {\n");
+
+        // TODO: if using glibc, use getopt.h to automate most of this
 
         // create c_var+"_isset" booleans
         for npi in &self.non_positional {
-            body.push_str(&format!("\tbool {}__isset = false;\n", npi.c_var));
+            body.push_str(&npi.decl_parse());
         }
 
         // push arg_count variable, which will be used for positional arguments
-        body.push_str("\tint arg_count = 0\n");
+        body.push_str("\tint arg_count = 0;\n");
 
         // primary loop npitem
         body.push_str("\tfor (int i = 1; i < argc; i++) {\n");
@@ -176,18 +224,45 @@ impl Spec {
         for pi in &self.positional {
             body.push_str(&pi.post_loop()); // TODO: Pass relative position index into pi.post_loop
         }
-
         for npi in &self.non_positional {
             body.push_str(&npi.post_loop());
         }
 
+        body.push_str("}\n");
         body
+    }
+    /// creates the main function in C.
+    fn c_main(&self) -> String {
+        let mut main = String::new();
+        main.push_str("int main(int argc, char **argv) {\n");
+
+        for pi in &self.positional {
+            main.push_str(&pi.decl())
+        }
+        for npi in &self.non_positional {
+            main.push_str(&npi.decl_main())
+        }
+
+        main.push_str("\n\tparse_args(argc, argv");
+        for pi in &self.positional {
+            main.push_str(&format!(", &{}", pi.c_var))
+        }
+        for npi in &self.non_positional {
+            main.push_str(&format!(", &{}", npi.c_var))
+        }
+        main.push_str(");\n\n");
+
+        main.push_str("\t/* TODO: call your code here */\n");
+        main.push_str("}\n");
+        main
     }
     /// generates argen.c which features the function argen.
     pub fn gen(&self) -> String {
-        let decl = self.func_decl();
-        let body = self.body();
-        format!("{}{}}}", decl, body)
+        let h = self.c_headers();
+        let usage = self.c_usage();
+        let body = self.c_parse_args();
+        let main = self.c_main();
+        format!("{}\n\n{}\n{}\n{}", h, usage, body, main)
         // TODO: Add Main function
     }
     /// writes generate C code to a writer.
